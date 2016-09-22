7e217d4cd602bdca47308f52bc5eab0df2c22bbe
dags- <dags@dags.me>
2016-09-22 13:32:54 +0100

    fix this

--- handler/statushandler.go
+++ handler/statushandler.go
@@ -41,13 +41,6 @@ func sendStatus(w http.ResponseWriter, r *http.Request, ip string, port string) 
 	status := getStatus(ip, port, uuid == "true")
 	response = status
 
-	if uuid == "true" {
-		players, _ := status["players"]
-		profiles := goquery.Profiles(players)
-		status.Put("players", profiles)
-		response = status
-	}
-
 	if keys != "" {
 		k := strings.Split(keys, ",")
 		status = status.Retain(k...)
@@ -62,7 +55,13 @@ func getStatus(ip string, port string, uuid bool) goquery.Data {
 	status, ok := manager.statusCache[key]
 	if !ok || time.Now().Sub(status.timestamp) > manager.statusExpire {
 		status = Status{goquery.GetStatus(ip, port), time.Now()}
+		if uuid {
+			players, _ := status.status["players"]
+			profiles := goquery.Profiles(players)
+			status.status.Put("players", profiles)
+		}
 		manager.statusCache[key] = status
+		fmt.Println("Refreshing status for", key)
 	}
 	return status.status
 }
