f5f660d049531b07735bd1b5a479c1f02353e4c3
dags- <dags@dags.me>
2016-09-22 13:15:46 +0100

    cache status and add stop

--- handler/statushandler.go
+++ handler/statushandler.go
@@ -6,7 +6,20 @@ import (
 	"github.com/dags-/goquery/query"
 	"fmt"
 	"strings"
+	"time"
 )
+
+type QueryManager struct {
+	statusCache  map[string]Status
+	statusExpire time.Duration
+}
+
+type Status struct {
+	status    goquery.Data
+	timestamp time.Time
+}
+
+var manager = QueryManager{make(map[string]Status), time.Duration(15 * time.Second)}
 
 func IpOnly(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -23,27 +36,33 @@ func IpAndPort(w http.ResponseWriter, r *http.Request) {
 
 func sendStatus(w http.ResponseWriter, r *http.Request, ip string, port string) {
 	var response interface{}
+	uuid, keys := r.FormValue("uuid"), r.FormValue("keys")
 
-	status := getStatus(ip, port)
+	status := getStatus(ip, port, uuid == "true")
 	response = status
 
-	if r.FormValue("uuid") == "true" {
+	if uuid == "true" {
 		players, _ := status["players"]
 		profiles := goquery.Profiles(players)
 		status.Put("players", profiles)
 		response = status
 	}
 
-	if val := r.FormValue("keys"); val != "" {
-		keys := strings.Split(val, ",")
-		status = status.Retain(keys...)
+	if keys != "" {
+		k := strings.Split(keys, ",")
+		status = status.Retain(k...)
 		response = status
 	}
 
 	fmt.Fprintf(w, goquery.ToJson(response, r.FormValue("pretty") == "true"))
 }
 
-func getStatus(ip string, port string) goquery.Data {
-	status := goquery.GetStatus(ip, port)
-	return status
+func getStatus(ip string, port string, uuid bool) goquery.Data {
+	key := fmt.Sprint(ip, ":", port, ":", uuid)
+	status, ok := manager.statusCache[key]
+	if !ok || time.Now().Sub(status.timestamp) > manager.statusExpire {
+		status = Status{goquery.GetStatus(ip, port), time.Now()}
+		manager.statusCache[key] = status
+	}
+	return status.status
 }
--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 	"github.com/dags-/goquery/handler"
 	"flag"
 	"fmt"
+	"bufio"
+	"os"
 )
 
 func main() {
+	go handleStop()
+
 	var port string
 	flag.StringVar(&port, "port", "8080", "Query port")
 	flag.Parse()
@@ -20,4 +24,18 @@ func main() {
 	r.HandleFunc("/status/{ip}/{port}", handler.IpAndPort)
 	r.HandleFunc("/head/{uuid}", handler.NewHeadServer())
 	http.ListenAndServe(":" + port, handlers.CORS()(r))
+}
+
+func handleStop() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		text, _ := reader.ReadString('\n')
+		if text == "stop\n" {
+			fmt.Println()
+			fmt.Println("Stopping...")
+			os.Exit(0)
+			break
+		}
+	}
 }
