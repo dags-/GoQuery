386df164b1297413baf131b9eef457b6ca9be767
dags- <dags@dags.me>
2017-05-02 16:43:12 +0100

    incase newrequest fails

--- discord/query.go
+++ discord/query.go
@@ -18,21 +18,26 @@ func GetStatus(serverId string) (Status, error) {
 
 	url := fmt.Sprintf(api, serverId)
 	request, err := http.NewRequest("GET", url, &bytes.Buffer{})
-	request.Header.Set("Connection", "close")
-
-	resp, err := http.DefaultClient.Do(request)
-	request.Close = true
-	request.Body.Close()
 
 	if err == nil {
-		decoder := json.NewDecoder(resp.Body)
-		err = decoder.Decode(&status)
-		resp.Body.Close()
-	}
+		request.Header.Set("Connection", "close")
 
-	if message, ok := status["message"]; ok {
-		err = errors.New(fmt.Sprint(message))
-		status = empty
+		response, err := http.DefaultClient.Do(request)
+		if err == nil {
+			decoder := json.NewDecoder(response.Body)
+			err = decoder.Decode(&status)
+
+			response.Close = true
+			response.Body.Close()
+		}
+
+		if message, ok := status["message"]; ok {
+			err = errors.New(fmt.Sprint(message))
+			status = empty
+		}
+
+		request.Close = true
+		request.Body.Close()
 	}
 
 	return status, err
